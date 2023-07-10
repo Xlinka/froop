878b6a92fed84cc725e42b6f199695bb947c16ea
xLinka <Oliver.dunnington@outlook.com>
2023-07-10 16:37:43 +0100

    Syntax Checking
    Simple Syntax Checker

--- froop.py
+++ froop.py
@@ -14,9 +14,19 @@ def tobf(s):
     s = s.replace("Froop? Froop!", "]")
     return s
 
+def check_syntax(code):
+    open_brackets = code.count("[")
+    close_brackets = code.count("]")
+    if open_brackets != close_brackets:
+        print(f"Syntax error: {open_brackets} opening brackets and {close_brackets} closing brackets!")
+        return False
+    return True
+
 try:
     with open(sys.argv[1], 'r') as f:
         program = tobf(f.read())
+        if not check_syntax(program):
+            sys.exit(1)
     d={'>':'p+=1\n','<':'p-=1\n','+':'n[p]+=1\n','-':'n[p]-=1\n','.':'print(chr(n[p]),end="")\n',',':'n[p]=ord(input())\n','[':'while n[p]:\n',']':''}
     s='n=[0]*32768\np=0\n'
     i=0
